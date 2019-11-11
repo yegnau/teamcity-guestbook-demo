cdc8d866deea08c6fd06c448b5774bb54336f8f9
Yegor Naumov <yegor.naumov@jetbrains.com>
2019-11-11 19:27:41 +0100

    greeting changed - again

--- frontend/src/components/app/app-root.js
+++ frontend/src/components/app/app-root.js
@@ -11,7 +11,7 @@ export default () => (
       <a href="/">
         <Logo glyph={jbLogo} size={Logo.Size.Size48} />
       </a>
-      <h2>{'Hi World!'}</h2>
+      <h2>{'Hi again!'}</h2>
     </Header>
     <div className="app-content">
       <Guestbook />
